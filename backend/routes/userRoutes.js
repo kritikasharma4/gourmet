ed175ef135f2beace408fd65977745602e4b5392
Kritika K. Sharma <128512600+kritikasharma4@users.noreply.github.com>
2025-03-02 10:34:59 +0530

    Delete backend directory

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("User routes working!");
-});
-
-export default router; // ✅ Ensure this is 'export default'
